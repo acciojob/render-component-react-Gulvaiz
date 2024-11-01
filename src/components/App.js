d7e7010df814e747d60c03c32508a3b14bd00ae6
Gulvaiz <gulvaizalam12@gmail.com>
2024-11-02 00:53:07 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Component from "./component";
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+   <Component/>
+    </div>
   )
 }
 
---
+++ src/components/Component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Component = () => {
+  return (
+    <div>
+      <h1>component rendering</h1>
+      <p>I am learning React. My life is getting better.</p>
+    </div>
+  )
+}
+
+export default Component
